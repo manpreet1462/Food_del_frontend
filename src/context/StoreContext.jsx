4111de346f46fde38baf98bc2e81d2d52c3da5b3
manpreet1462 <80613332+manpreet1462@users.noreply.github.com>
2024-07-24 15:36:07 +0530

    frontend URL changed

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext= createContext(null);
 
 const StoreContextProvider=(props)=>{
     const[cartItems,setCartItems]=useState({});
-    const url="http://localhost:8080"
+    const url="https://food-del-backend-t63h.onrender.com"
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([])
 
